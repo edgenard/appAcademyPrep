ad2735bb194c1db8c99b20c8ba12e10af150e20f
Emmanuel Genard <edgenard@gmail.com>
2014-10-05 00:54:59 -0400

    Finished chapter 7 and 8

--- learnToProgramExercises.rb
+++ learnToProgramExercises.rb
@@ -104,8 +104,7 @@ Chapter 3: Letters                                    page 13
 1. 99 Bottles of Beer on the Wall. Write a program that prints out the lyrics to the beloved classic.
 
 2. Deaf Grandma. Whatever you say (type in), she should respond with this:
-HUH?! SPEAK UP, SONNY!
-unless you shout it(type it in all capitals). If you shout, she can hear you (or at least she thinks so) and yells back:
+HUH?! SPEAK UP, SONNY! unless you shout it(type it in all capitals). If you shout, she can hear you (or at least she thinks so) and yells back:
 NO NOT SINCE 1938!
 To make your program really believable, have Grandma shout a different year each time, maybe any year at random between 
 1930 and 1950. You can't stop talking to Grandma until you shout BYE.
@@ -117,6 +116,7 @@ three times but not in a row, you should still be talking to Grandma.
 4. Leap years. Write a program that asks for a starting year and an ending year and then puts all the leap years
 between them (and including them, if they are also leap years). Leap years are years divisible by 4. However, years divisible
 by 100 are not leap years unless they are also divisible by 400.
+
 =end
 
 #99 Bottles of Beer
@@ -139,49 +139,122 @@ by 100 are not leap years unless they are also divisible by 400.
 
 
 #Deaf Grandma and Deaf Grandma extended
-puts "Speak to your Nana"
-nana_huh = "HUH?! SPEAK UP SONNY!"
-nana_no = "NO! NOT SINCE "
+# puts "Speak to your Nana"
+# nana_huh = "HUH?! SPEAK UP SONNY!"
+# nana_no = "NO! NOT SINCE "
+#
+# while true
+#   year = 1900
+#   say_to_nana = gets.chomp
+#   while !(say_to_nana == say_to_nana.upcase)
+#     puts nana_huh
+#     say_to_nana = gets.chomp
+#   end
+#   if say_to_nana == "BYE"
+#     bye_count = 1
+#     while bye_count < 3
+#       puts nana_huh
+#       say_to_nana = gets.chomp
+#       if say_to_nana != "BYE"
+#         bye_count = bye_count - 1
+#       else
+#         bye_count = bye_count + 1
+#       end
+#     end
+#     puts "OH, OK...BYE"
+#     break
+#   elsif say_to_nana == say_to_nana.upcase
+#     rando = rand(30)
+#     if rando <= 8
+#       year = (year + rando + 30).to_s + "!"
+#     else
+#       year = (year + rando + 21).to_s + "!"
+#     end
+#     puts nana_no + year
+#   end
+# end
 
-while true
-  year = 1900
-  say_to_nana = gets.chomp
-  while !(say_to_nana == say_to_nana.upcase)
-    puts nana_huh
-    say_to_nana = gets.chomp
-  end
-  if say_to_nana == "BYE"
-    bye_count = 1
-    while bye_count < 3
-      puts nana_huh
-      say_to_nana = gets.chomp
-      if say_to_nana != "BYE"
-        bye_count = bye_count - 1
-      else
-        bye_count = bye_count + 1
-      end  
-    end
-    puts "OH, OK...BYE"
-    break
-  elsif say_to_nana == say_to_nana.upcase
-    rando = rand(30)
-    if rando <= 8
-      year = (year + rando + 30).to_s + "!"
-    else
-      year = (year + rando + 21).to_s + "!"
-    end
-    puts nana_no + year
+#Leap Year
+# puts "Let me tell you all the leap years between two years."
+# puts "What year do you want to start with?"
+# start_year = gets.chomp
+# puts "Good, I will start counting leap years from the year " + start_year
+# puts "What year do you want me to end with?"
+# end_year = gets.chomp
+# puts "I'll stop counting when I get to " + end_year
+# start_year_int = 0
+# end_year_int = 0
+# if start_year > end_year
+#   start_year_int = end_year.to_i
+#   end_year_int = start_year.to_i
+# else
+#   start_year_int = start_year.to_i
+#   end_year_int = end_year.to_i
+# end
+#
+# while start_year_int <= end_year_int
+#   if start_year_int % 100 == 0
+#     if start_year_int % 400 == 0
+#       puts start_year_int.to_s + " is a leap year"
+#     end
+#   elsif start_year_int % 4 == 0
+#     puts start_year_int.to_s + " is a leap year"
+#   end
+#   start_year_int += 1
+# end
+#
+# puts "That's all the leap years between " + start_year + " and " + end_year
+
+=begin
+8.3
+1. Building and sorting an array. Write the program we talked about at the 
+beginning of this chapter, one that asks us to type as many words as we want 
+(one word per line, continuing until we just press Enter on an empty line) and 
+then repeats the words back to us in alphabetical order. Make sure to test your
+program thor- oughly; for example, does hitting Enter on an empty line always
+exit your program? Even on the first line? And the second? Hint: There’s a 
+lovely array method that will give you a sorted version of an array: sort. Use it!
+
+2. Table of contents, revisited. Rewrite your table of contents program on 
+page 36. Start the program with an array holding all of the information for 
+your table of contents (chapter names, page numbers, and so on). Then print out
+the information from the array in a beautifully formatted table of contents.
+=end
+
+#Building and Sorting an array.
+# puts "Write as many words as you want and I'll alphabetize them for you!"
+# puts "When you're done, just press <<ENTER>> on an empty line"
+# array = []
+# while true
+#   word = gets.chomp
+#   if word == '' && array.length < 3
+#     puts "I need more words"
+#   elsif word == '' && array.length > 3
+#     break
+#   else
+#     array<< word
+#   end
+# end
+#
+# array.length.times do |index|
+#   array[index] = array[index].downcase
+# end
+# array = array.sort
+# puts array
+
+#Table of Contents
+toc = [["Table of Contents"], ["Chapter 1: Getting Started", "page 1"], ["Chapter 2: Numbers", "page 9"],
+["Chapter 3: Letters", "page 13"] ]
+
+line_width = 50
+
+toc.length.times do |index|
+  if index == 0
+    puts toc[index][index].center(line_width)
+  else
+    puts toc[index][0].ljust((line_width * 0.66).to_i) + toc[index][1].rjust((line_width * 0.33).to_i)
   end
 end
-
-
-
-  
-  
-
-
-
-
 
 
 
