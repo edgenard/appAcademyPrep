283692f176032e15597d1b68759f65b67f2b9e79
Emmanuel Genard <edgenard@gmail.com>
2014-11-06 13:16:31 -0500

    Passed the tests for 8

--- test-first-ruby-master/lib/08_book_titles.rb
+++ test-first-ruby-master/lib/08_book_titles.rb
@@ -1,3 +1,19 @@
 class Book
-  # TODO: your code goes here!
+  attr_accessor :title
+  def title
+    non_capitalized_words = %w{the and an a in of}
+    offset = 0
+    @title.gsub!(/\w+/) do |match|
+      if @title.index(match, offset) == 0
+        offset = 1
+        match.capitalize 
+      elsif non_capitalized_words.include?(match) 
+        match 
+      elsif match == "i"
+        match.capitalize
+      else
+        match.capitalize
+      end
+    end
+  end
 end
