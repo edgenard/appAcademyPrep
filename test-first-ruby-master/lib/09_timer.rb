9a3b1a72f0fd565b13c7e8279d0fc55ca1cd8766
Emmanuel Genard <edgenard@gmail.com>
2014-11-06 13:41:28 -0500

    Working on tests for 9

---
+++ test-first-ruby-master/lib/09_timer.rb
@@ -0,0 +1,14 @@
+class Timer
+  attr_accessor :seconds
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    hours = @seconds / 3600
+    minutes = (@seconds - (hours * 3600)) / 60
+    seconds = @seconds - (hours * 3600) - (minutes * 60)
+    "#{hours}:#{minutes}:#{seconds}" 
+  end
+  
+end
