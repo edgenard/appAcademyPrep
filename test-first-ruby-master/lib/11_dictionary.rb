48328c679c8d394df962630447f14dc73ff39f4d
Emmanuel Genard <edgenard@gmail.com>
2014-11-10 13:35:19 -0500

    passed the test for 11

--- test-first-ruby-master/lib/11_dictionary.rb
+++ test-first-ruby-master/lib/11_dictionary.rb
@@ -4,11 +4,36 @@ class Dictionary
     @entries = {}
   end
 
-  def add(entry = {})
-    @entries[entry.keys[0]] = entry[entry.keys[0]]
+  def add(*entry)
+    entry = entry[0] 
+    entry.is_a?(Hash) ? @entries.merge!(entry) : @entries.store(entry,nil)
   end
 
   def keywords
-    @entries.keys 
+    @entries.keys.sort 
   end
+
+  def include?(key)
+    @entries.has_key?(key)
+  end
+
+  def find(word)
+    @entries.select {|key, value| key.start_with?(word) }
+  end
+
+  def printable
+    keywords = @entries.keys.sort
+    printout = ""
+    keywords.each do |word|
+      printout = printout + "[#{word}] \"#{@entries[word]}\"\n"
+    end
+    printout.strip!
+  end
+
+
+
 end
+
+
+
+
