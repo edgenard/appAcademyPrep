4d45e4d22815cfaea92b64a3ad3e158b83174c34
Emmanuel Genard <edgenard@gmail.com>
2014-11-15 22:41:40 -0500

    working on event manager

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,8 +1,32 @@
+require "csv"
+require "sunlight/congress"
+require "erb"
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+def legislator_by_zipcode(zipcode)
+  Sunlight::Congress::Legislator.by_zipcode(zipcode)
+end
+
 puts "Event Manager Initialized!"
 
-lines = File.readlines "event_attendees.csv"
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
 
-lines.each do |line|
-  columns = line.split(",")
-  p columns
+contents = CSV.open "../event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = legislator_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  puts form_letter
 end
+
+
