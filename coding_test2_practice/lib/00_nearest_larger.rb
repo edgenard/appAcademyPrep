95d3fe649a43102b48ac13f4da0a40012793260b
Emmanuel Genard <edgenard@gmail.com>
2014-11-26 17:47:26 -0500

    Finished first practice for coding test 2

--- coding_test2_practice/lib/00_nearest_larger.rb
+++ coding_test2_practice/lib/00_nearest_larger.rb
@@ -1,2 +1,30 @@
-def nearest_larger(arr, idx)
+def closest(arr, point)
+  closest = []
+  difference = (point - arr[0]).abs
+  arr.each do |value|
+    if (point - value).abs < difference
+      closest << value
+    end
+  end
+  if closest.empty?
+    return arr
+  end
+  closest
 end
+
+
+def nearest_larger(arr, idx)
+  index_larger = []
+  arr.each_with_index do |value, index|
+    if value > arr[idx]
+      index_larger << index
+    end
+  end
+  if index_larger.empty?
+    return nil
+  end
+  index_larger = closest(index_larger, idx)
+  index_larger.sort[0]
+end
+
+p nearest_larger([2, 6, 9, 4, 8], 3)
