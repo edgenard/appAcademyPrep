32355d803f4f85846b109beadd979a2ff9bcf553
Emmanuel Genard <edgenard@gmail.com>
2014-12-01 14:44:37 -0500

    Working on 4

--- coding_test2_practice/lib/04_wonky_coins.rb
+++ coding_test2_practice/lib/04_wonky_coins.rb
@@ -1,2 +1,13 @@
 def wonky_coins(n)
+  coins = []
+  if n == 0
+    coins << 0
+  else
+    coins = [wonky_coins(n/2), wonky_coins(n/3), wonky_coins(n/4)]
+  end
+  p coins
+  coins.size
+  
 end
+
+p wonky_coins(5)
