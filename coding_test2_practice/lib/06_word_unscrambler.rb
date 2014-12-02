a944946a2d79f1e14eeea024437db52317ca6a3d
Emmanuel Genard <edgenard@gmail.com>
2014-12-02 17:27:58 -0500

    Passed test for 6 and 7

--- coding_test2_practice/lib/06_word_unscrambler.rb
+++ coding_test2_practice/lib/06_word_unscrambler.rb
@@ -1,2 +1,13 @@
 def word_unscrambler(str, words)
+  words.select do |word|
+    is_anagram?(str, word)
+  end
+end
+
+def is_anagram?(word1, word2)
+  if word1.split(//).sort == word2.split(//).sort
+    return true  
+  end
+  false
+  
 end
--- coding_test2_practice/lib/07_rectangle_intersection.rb
+++ coding_test2_practice/lib/07_rectangle_intersection.rb
@@ -1,2 +1,25 @@
 def rec_intersection(rect1, rect2)
+  leftmost, rightmost = order_rect(rect1, rect2)
+  if !rightmost[0][0].between?(leftmost[0][0], leftmost[1][0])
+    return nil
+  end
+  if rightmost[1][0].between?(leftmost[0][0], leftmost[1][0]) && 
+    rightmost[1][1].between?(leftmost[0][1], leftmost[1][1])
+    return rightmost
+  end
+
+  bottom_left = [[leftmost[0][0], rightmost[0][0]].max,[leftmost[0][1], rightmost[0][1]].max ]
+  top_right = [[leftmost[1][0], rightmost[1][0]].min, [leftmost[1][1], rightmost[1][1]].min]
+
+  [bottom_left, top_right]
 end
+
+
+def order_rect(rect1, rect2)
+  if rect2[0][0].between?(rect1[0][0], rect1[1][0])
+    return rect1, rect2
+  else
+    return rect2, rect1
+  end
+end
+
