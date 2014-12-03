57928f9ff6fccac0b21cef485f90bada2f919476
Emmanuel Genard <edgenard@gmail.com>
2014-12-03 11:54:28 -0500

    refactored test_00

--- coding_test2_practice/lib/00_nearest_larger.rb
+++ coding_test2_practice/lib/00_nearest_larger.rb
@@ -1,29 +1,40 @@
-def closest(arr, point)
-  closest = []
-  difference = (point - arr[0]).abs
-  arr.each do |value|
-    if (point - value).abs < difference
-      closest << value
-    end
-  end
-  if closest.empty?
-    return arr
-  end
-  closest
-end
-
 
 def nearest_larger(arr, idx)
-  index_larger = []
-  arr.each_with_index do |value, index|
-    if value > arr[idx]
-      index_larger << index
-    end
+  right = larger_right(arr, idx)
+  left = larger_left(arr, idx)
+  if right.nil? && !left.nil?
+    return left
+  elsif !right.nil? && left.nil?
+    return right
   end
-  if index_larger.empty?
+  if right.nil? && left.nil?
     return nil
   end
-  index_larger = closest(index_larger, idx)
-  index_larger.sort[0]
+  if (left-idx) > (idx - right)
+    return right
+  else
+    return left
+  end
 end
+
+def larger_right(arr, idx)
+  arr[idx..arr.length].each do |value|
+    if value > arr[idx]
+      return arr.index(value)
+    end
+  end
+  nil
+end
+
+def larger_left(arr, idx)
+  arr[0..idx].reverse_each do |value|
+    if value > arr[idx]
+      return arr.index(value)
+    end
+  end
+  nil
+end
+
+
+
 
