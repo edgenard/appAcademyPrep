0e5349931cc093eb1628cc3223dc8eab08849fbf
Emmanuel Genard <edgenard@gmail.com>
2014-12-03 10:38:38 -0500

    Passed test 8

--- coding_test2_practice/lib/08_bubble_sort.rb
+++ coding_test2_practice/lib/08_bubble_sort.rb
@@ -1,2 +1,24 @@
-def bubble_sort(arr)
+def bubble_sort(arr, stop=arr.length-1)
+  if arr.empty? || arr.length == 1
+    return arr
+  end
+  swapped = false
+  index = 0
+  while index + 1 <= stop
+    left = arr[index]
+    right = arr[index + 1]
+    if left > right
+      arr[index] = right
+      arr[index+1] = left
+      swapped = true      
+    end
+    index += 1
+  end
+
+  if swapped
+    stop = stop - 1
+    bubble_sort(arr, stop)
+  else
+    return arr
+  end
 end
