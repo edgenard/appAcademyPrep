7e5624276c402937648dba08a71b49a1fd1975a3
Emmanuel Genard <edgenard@gmail.com>
2014-12-03 12:08:31 -0500

    little change to 02

--- coding_test2_practice/lib/02_letter_count.rb
+++ coding_test2_practice/lib/02_letter_count.rb
@@ -1,6 +1,6 @@
 def letter_count(str)
   letter_count = {}
-  str.gsub(" ", "").each_char do |chr|
+  str.gsub(/\s+/, "").each_char do |chr|
     letter_count[chr] = str.count(chr)
   end
   letter_count
